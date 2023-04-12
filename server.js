d49f1bf4350de684661dfc1dbaeff9c5c0ef3367
Eric Iwan <eric.iwan@mail.com>
2023-04-12 05:57:26 +0000

    Many changes

--- server.js
+++ server.js
@@ -3,20 +3,38 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
+const { OAuth2Client } = require('google-auth-library');
+const client_id = 'YOUR_CLIENT_ID';
+const client_secret = 'YOUR_CLIENT_SECRET';
+const redirect_uri = 'YOUR_REDIRECT_URI';
+const oauth2Client = new OAuth2Client(client_id, client_secret, redirect_uri);
+const uploadRouter = require('./router');
+const getDriveService = require('./service.js');
+
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
+app.use(uploadRouter);
+require('dotenv').config();
+
+
 
 app.get('/', (req, res) => { 
     res.render("login", {title: "This is a title"});
 });
 
+app.post("/upload", upload.single('file'), async (req,res)  => {
+  const drive = getDriveService();
+
+
+})
+
 app.post("/login", (req,res) => {
   console.log(req.body)
   if(req.body.id == "12345"){
-    res.send({valid: true})
+    return res.send({valid: true})
   }
   return res.send({error: "Incorrect login information haha"})
 })
@@ -28,3 +46,7 @@ app.post("/login", (req,res) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+
+
+//figure out how to not need a file key for service.js
