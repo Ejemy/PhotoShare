aaaa01c5a6087ff3460aa3457bbc6e7e81381dcb
Eric Iwan <eric.iwan@mail.com>
2023-04-14 07:25:21 +0000

    ulh

--- service.js
+++ service.js
@@ -5,18 +5,30 @@ const axios = require('axios');
 
 const keyUrl = process.env.FILEKEY;
 
-const response = await axios.get(keyUrl);
 
-const key = response.data;
+const getCredentials = async () => {
+  try {
+    const response = await axios.get(keyUrl, {
+      headers: {
+        "X-Master-Key" : process.env.API_KEY
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching credentials: ${error}`);
+    throw error;
+  }
+};
 
-const getDriveService = () => {
-  const KEYFILEPATH = key;
+const getDriveService = async () => {
+  const KEYFILEPATH = await getCredentials();
   const SCOPES = ['https://www.googleapis.com/auth/drive'];
 
-  const auth = new google.auth.GoogleAuth({
-    keyFile: KEYFILEPATH,
+  const auth = await new google.auth.GoogleAuth({
+    KeyFile: JSKEYFILEPATH,
     scopes: SCOPES,
   });
+  console.log("Auth: ", auth)
   const driveService = google.drive({ version: 'v3', auth });
   return driveService;
 };
